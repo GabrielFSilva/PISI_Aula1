e1d9cfb64fa1d057a3804bd06ee10849de429483
GabrielFSilva <gabrielsilva.prog@gmail.com>
2019-03-13 15:29:25 -0300

    Initial commit

---
+++ Assets/Scripts/Intro/IntroManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Q Pressed");
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Left Button Pressed");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //SceneManager.LoadScene(1);
+            //SceneManager.LoadScene("1-Behaviours");
+        }
+
+    }
+}
